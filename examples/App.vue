727285375c63c701be2a4f944f9bc127051b2ae5
陈炬 <Ju.Chen@geely.com>
2022-11-30 17:12:11 +0800

    引入element-plus

--- examples/App.vue
+++ examples/App.vue
@@ -3,10 +3,14 @@
  * @Author: chenju
  * @Date: 2022-11-30 15:55:25
  * @LastEditors: chenju
- * @LastEditTime: 2022-11-30 16:08:49
+ * @LastEditTime: 2022-11-30 17:06:07
 -->
 <template>
   <div>
+      <el-button>测试</el-button>
+      <el-button type="primary">测试</el-button>
+      <el-button type="warning">测试</el-button>
+      <el-button type="danger">测试</el-button>
       <my-button />
   </div>
 </template>
--- examples/main.ts
+++ examples/main.ts
@@ -3,14 +3,16 @@
  * @Author: chenju
  * @Date: 2022-11-30 15:55:34
  * @LastEditors: chenju
- * @LastEditTime: 2022-11-30 16:08:46
+ * @LastEditTime: 2022-11-30 17:09:19
  */
 import {createApp} from 'vue'
 import App from './App.vue'
-import { Button } from 'my-ui2'
+import { Button } from '@xfchenju/my-ui2'
+// import ElementPlus from 'element-plus';
+// import 'element-plus/dist/index.css'
 
 const app = createApp(App)
 
+// app.use(ElementPlus)
 app.component('my-button', Button)
-
 app.mount('#app')
