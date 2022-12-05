bddbc17b4ccaf1fc2923d1233e2ad8481c8c7e4c
陈炬 <Ju.Chen@geely.com>
2022-12-05 10:39:07 +0800

    修改

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,12 @@
 
 
+## [1.2.0](https://github.com/xfchenju/my-ui2/compare/1.1.0...1.2.0) (2022-12-02)
+
+
+### Features
+
+* 更新utils库 ([c7c7e43](https://github.com/xfchenju/my-ui2/commit/c7c7e4397437bdbf2ebf426b4f47a74c9e957411))
+
 * feat: 更新utils库 (c7c7e43)
 
 ## 1.1.0 (2022-12-02)
--- packages/utils/script/publish/index.ts
+++ packages/utils/script/publish/index.ts
@@ -3,24 +3,16 @@
  * @Author: chenju
  * @Date: 2022-12-02 16:55:39
  * @LastEditors: chenju
- * @LastEditTime: 2022-12-02 16:58:25
+ * @LastEditTime: 2022-12-05 10:35:41
  */
-import { src, dest } from 'gulp'
 import { PKG_NAME } from '../config'
 import { componentPath } from '../utils/paths'
 import { run } from '../utils/run'
 
-//复制
-export const copyPackage = async () => {
-  return src(`${componentPath}/transitpkg/**`).pipe(dest(`${componentPath}/${PKG_NAME}/`));
-};
-
 //发布任务
 export const publish = async () => {
   //先给transitpkg升个版本
-  await run('pnpm version patch', `${componentPath}/transitpkg`)
-  //复制到dist目录
-  await copyPackage()
+  await run('pnpm version patch', `${componentPath}/${PKG_NAME}`)
   //在dist下执行发布命令
   await run('npm publish', `${componentPath}/${PKG_NAME}`)
   // run('pnpm publish')
