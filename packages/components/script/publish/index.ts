66696d3c051b64f24988469b0be9a976efbd4ff0
陈炬 <Ju.Chen@geely.com>
2022-12-05 11:24:50 +0800

    修改

--- packages/components/script/publish/index.ts
+++ packages/components/script/publish/index.ts
@@ -3,7 +3,7 @@
  * @Author: chenju
  * @Date: 2022-12-02 16:55:39
  * @LastEditors: chenju
- * @LastEditTime: 2022-12-02 16:58:25
+ * @LastEditTime: 2022-12-05 11:08:22
  */
 import { src, dest } from 'gulp'
 import { PKG_NAME } from '../config'
@@ -11,17 +11,17 @@ import { componentPath } from '../utils/paths'
 import { run } from '../utils/run'
 
 //复制
-export const copyPackage = async () => {
-  return src(`${componentPath}/transitpkg/**`).pipe(dest(`${componentPath}/${PKG_NAME}/`));
-};
+// export const copyPackage = async () => {
+//   return src(`${componentPath}/transitpkg/**`).pipe(dest(`${componentPath}/${PKG_NAME}/`));
+// };
 
 //发布任务
 export const publish = async () => {
   //先给transitpkg升个版本
-  await run('pnpm version patch', `${componentPath}/transitpkg`)
+  await run('pnpm version patch', `${componentPath}/${PKG_NAME}`)
   //复制到dist目录
-  await copyPackage()
+  // await copyPackage()
   //在dist下执行发布命令
-  await run('npm publish', `${componentPath}/${PKG_NAME}`)
+  await run('pnpm publish --access=public', `${componentPath}/${PKG_NAME}`)
   // run('pnpm publish')
 }
